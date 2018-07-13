014a9d716bb13edb8622b96089b68bea57cd70f8
Jeffrey D. Muehlbauer <jmuehlbauer@usgs.gov>
2018-07-13 16:59:04 -0700

    Updated R code to merge env and sample data.

--- RCode-LF_Drift.r
+++ RCode-LF_Drift.r
@@ -20,10 +20,15 @@ if('dataRetrieval' %in% rownames(installed.packages()) == FALSE) {
 	install.packages('dataRetrieval', repos = 'https://cran.cnr.berkeley.edu')
 }
 
+# Check for data.table package, install if not found
+if('data.table' %in% rownames(installed.packages()) == FALSE) {
+	install.packages('data.table', repos = 'https://cran.cnr.berkeley.edu')
+}
+
 # Load requisite packages
 require(foodbase)
 require(dataRetrieval)
-
+require(data.table)
 
 # Get data ---------------------------------------------------------------------
 
@@ -55,10 +60,45 @@ e1 <- e0[, c('dateTime', 'X_00010_00000', 'X_00060_00000', 'X_00065_00000',
 # Write environmental data locally to save time for future runs 
 dbdir <- paste0(find.package('foodbase'),'/Data')
 	# Location of the foodbase package "Data" folder.
-write.csv(e0, paste0(dbdir, '/GageData.csv'), row.names = FALSE)
+write.csv(e1, paste0(dbdir, '/GageData.csv'), row.names = FALSE)
 
 # Read in these data (start from here after first code run)
-e0 <- read.csv(paste0(dbdir, '/GageData.csv'))
+e1 <- read.csv(paste0(dbdir, '/GageData.csv'))
 
 
-	
+# Clean up data ----------------------------------------------------------------
+
+# Convert DateTimes to POSIX
+e2 <- e1
+	e2$DateTime <- strptime(e1$DateTime, format = '%Y-%m-%d %H:%M:%S', tz = 'MST')
+
+# Merge sample and environmental data
+d3 <- d2
+e2T <- e2[!is.na(e2$Temperature),]
+e2S <- e2[!is.na(e2$SpC),]
+drifttime0 <- strptime(paste(d3$Samples$Date, d3$Samples$TimeBegin), 
+	format = '%Y-%m-%d %H:%M:%S', tz = 'MST')
+d3$Samples <- d2$Samples[order(drifttime0),]
+	d3$Samples$SpC <- d3$Samples$Stage <- d3$Samples$Discharge <- 
+	d3$Samples$Temperature <- NA
+drifttime <- suppressWarnings(data.table(Drift = drifttime0))
+	drifttime[, merge:= Drift]
+envtime <- suppressWarnings(data.table(Env = e2$DateTime))
+	envtime[, merge:= Env]
+envtimeT <- suppressWarnings(data.table(Env = e2T$DateTime))
+	envtimeT[, merge:= Env]
+envtimeS <- suppressWarnings(data.table(Env = e2S$DateTime))
+	envtimeS[, merge:= Env]
+setkeyv(drifttime, c('merge'))
+setkeyv(envtime, c('merge'))
+setkeyv(envtimeT, c('merge'))
+setkeyv(envtimeS, c('merge'))
+closest <- envtime[drifttime, roll = 'nearest']
+closestT <- envtimeT[drifttime, roll = 'nearest']
+closestS <- envtimeS[drifttime, roll = 'nearest']
+d3$Samples[, c('Discharge', 'Stage')] <- 
+	e2[match(closest$Env, as.POSIXct(e2$DateTime)), c('Discharge', 'Stage')]
+d3$Samples[, 'Temperature'] <- 
+	e2T[match(closestT$Env, as.POSIXct(e2T$DateTime)), 'Temperature']
+d3$Samples[, 'SpC'] <- 
+	e2S[match(closestS$Env, as.POSIXct(e2S$DateTime)), 'SpC']
